3be89b659903e971133e5836d46dc31a734bf37d
Victor Paranhos <victor_paranhos@hotmail.com>
2022-03-02 11:37:20 -0300

    Update script.js

--- funcoes/script.js
+++ funcoes/script.js
@@ -4,26 +4,23 @@ const horas = document.querySelector(".horas");
 const minutos = document.querySelector(".minutos");
 const segundos = document.querySelector(".segundos");
 
-const NewAno = "1 Jan 2023"
+const NewAno = "04 Mar 2022"
 
 
 function contdown(){
     const novaData = new Date(NewAno);
-    const current = new Date()
+    const DataAtual = new Date()
 
-    const TotalSegundos = (novaData - current) / 1000
+    const TotalSegundos = (novaData - DataAtual) / 1000
     const days = Math.floor(TotalSegundos / 3600 / 24);
     const hor = Math.floor(TotalSegundos / 3600 ) % 24;
     const minutes = Math.floor(TotalSegundos / 60) % 60;
     const seconds = Math.floor(TotalSegundos) % 60;
 
-    dias.innerHTML = days;
+    dias.innerHTML = format(days);
     horas.innerHTML = format(hor);  
     minutos.innerHTML = format(minutes);
-    segundos.innerHTML = format(seconds);
-
-    
-   
+    segundos.innerHTML = format(seconds);   
 }
 
 
